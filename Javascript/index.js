ba86234bff1ec6e71686dd4202dd9bc3074414d8
abhishek <abhishekprasad7250@gmail.com>
2025-01-30 22:55:58 +0530

    push

---
+++ Javascript/index.js
@@ -0,0 +1,19 @@
+// const profile = {
+//     name :"abhishek",
+//     age : 23,
+//     gender: "male",
+//     address: "delhi",
+//     occupation: "student",
+//     hobbies: ["cricket", "football", "coding"],
+//     }
+// console.log([typeof profile.age]);
+
+const parkerpen ={
+    title : "blue pen",
+    color : "blue",
+    price : 10,
+    brand : "parker",
+    material : "plastic",
+    offer : 5,
+};
+console.log(parkerpen);
